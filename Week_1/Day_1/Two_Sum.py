5ae6acd5b8cdb3b59220a15f4030fef617d9ea56
Daiji-san <devyanshshukla255@gmail.com>
2026-06-01 13:15:47 +0530

    Add problem link and description to Two_Sum.py
    Added problem link and description for Two Sum.

--- Week_1/Day_1/Two_Sum.py
+++ Week_1/Day_1/Two_Sum.py
@@ -1,1 +1,11 @@
+#Problem Link: https://leetcode.com/problems/two-sum/
+#Description: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
+class Solution(object):
+    def twoSum(self, nums, target):
+        num_tab = {}
+        for i, num in enumerate(nums):
+            second = target - num
+            if second in num_tab:
+                return [num_tab[second], i]
+            num_tab[num] = i
