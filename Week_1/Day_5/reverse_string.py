99564b47b84f09d814d318c71ae760a889bc957c
Daiji-san <devyanshshukla255@gmail.com>
2026-06-05 20:27:59 +0530

    Add reverseString function to reverse a character array
    Implement a function to reverse a string in-place.

---
+++ Week_1/Day_5/reverse_string.py
@@ -0,0 +1,12 @@
+#Problem Link --> https://leetcode.com/problems/reverse-string/description/
+#Description --> Write a function that reverses a string. The input string is given as an array of characters s.
+#                You must do this by modifying the input array in-place with O(1) extra memory.
+
+class Solution(object):
+    def reverseString(self, s):
+        l = 0
+        r = len(s)-1
+        while l<=r:
+            s[l], s[r] = s[r], s[l]
+            l+=1
+            r-=1
